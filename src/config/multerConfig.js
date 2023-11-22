f68c86b08c8d466a875b40ca6bb7f0eba273b262
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-11-22 20:26:31 -0300

    add file format validation

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -3,6 +3,13 @@ import { extname, resolve } from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+      return cb(new multer.MulterError('File is not an image'));
+    }
+    return cb(null, true);
+  },
+
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,8 +1,16 @@
-import Aluno from '../models/Aluno';
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
 
+const upload = multer(multerConfig).single('photo');
 class PhotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({ errors: [err.code] });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
